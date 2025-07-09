8ff5fd875ba5075b9eefe9f6cc311420d21d47fb
raymondlin7510 <raymond.lin7510@berkeley.edu>
2025-07-09 11:56:59 -0700

    Added in landmarks and the try and except condition

--- main.py
+++ main.py
@@ -8,9 +8,26 @@ import os
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0])
+    angle = np.abs(radians * 180./np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+    
+    return angle
+
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 
     cap = cv2.VideoCapture(0)
+    cap.set(3, 2000)
+    cap.set(4, 1000)
+    cap.set(10, 100)
+
     while cap.isOpened():
         ret, frame = cap.read()
 
@@ -25,15 +42,21 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True
         image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+        except:
+            pass
+            
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                )
+                                    mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                    mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                    )
 
         cv2.imshow('Mediapipe Feed', image)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-    # monkey ah add
 
     cap.release()
     cv2.destroyAllWindows()
+
